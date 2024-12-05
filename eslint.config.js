c7f890f6b9d484d7514026ecc465622c7fba1203
MuyianKing <muyianking@163.com>
2024-12-05 13:39:24 +0800

    feat: :sparkles: 增加打包和发布命令

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,10 @@ export default antfu(
     vue: true,
     isInEditor: false,
     formatters: true,
-    ignores: ['**/public/**'],
+    ignores: [
+      '**/public/**',
+      'package.json',
+    ],
   },
   {
     rules: {
--- script/publish.js
+++ script/publish.js
@@ -1,6 +1,8 @@
 import child_process from 'node:child_process'
 import path from 'node:path'
+import process from 'node:process'
 import { fileURLToPath } from 'node:url'
+import fsExtra from 'fs-extra'
 import getObjectFromJson from './utils/getObjectFromJson.js'
 
 const __dirname = fileURLToPath(import.meta.url)
@@ -10,7 +12,20 @@ async function publish() {
   const package_path = path.resolve(__dirname, _path)
   const _config = getObjectFromJson(package_path)
 
-  const version = `v${_config.version}`
+  let version = `v${_config.version}`
+  const params = getParams()
+
+  // 如果外部传入版本号则以外部为准
+  if (params.v) {
+    version = `v${params.v}`
+    _config.version = params.v
+
+    fsExtra.outputFile(
+      package_path,
+      JSON.stringify(_config, '', '\t'),
+      'utf-8',
+    )
+  }
 
   try {
     await exec('pnpm log')
@@ -34,6 +49,20 @@ function exec(cmd) {
       }
     })
   })
+}
+
+// 获取参数
+function getParams() {
+  const params = {}
+  process.argv.forEach((item) => {
+    item = item.split('=')
+
+    if (item.length === 2) {
+      params[item[0]] = item[1]
+    }
+  })
+
+  return params
 }
 
 publish()
